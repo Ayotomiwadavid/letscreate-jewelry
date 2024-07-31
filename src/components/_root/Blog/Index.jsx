9088281dc2ea9ae6e46e52df18b7d6420228e4e2
Techthoth <ayotomiwadavid@proton.me>
2024-07-31 18:00:09 +0100

    saving progress

--- src/components/_root/Blog/Index.jsx
+++ src/components/_root/Blog/Index.jsx
@@ -10,7 +10,7 @@ const BlogPage = () => {
     <section className="">
       <Navbar />
       <Hero title='Blogs'/>
-      <main className="py-[50px] px-[5vw] grid grid-cols-4 w-full place-content-start place-items-center gap-6">
+      <main className="py-[50px] grid grid-cols-1 md:px-5vw md:grid-cols-4 w-full place-content-start place-items-center gap-4">
        {
         Blogs.map((blog) => (<Blogcard blogDescription={blog.blogDescription} blogImage={blog.blogImage} BlogTitle={blog.blogTitle}/>))
        }
